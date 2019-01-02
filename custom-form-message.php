5b6e6cc9ac6728c4400a145df1d82d016ff480f8
Danny van Kooten <dannyvankooten@gmail.com>
2019-01-02 16:45:09 +0100

    add snippet for custom form message

---
+++ custom-form-message.php
@@ -0,0 +1,13 @@
+<?php 
+
+/* 
+Default message keys:
+    success' 
+    invalid_email'
+    required_field_missing
+    error
+*/
+
+add_filter( 'hf_form_message_success', function( $message ) {
+    return 'Your custom success message here';
+});
