82f8a3e65ed4a62ad5e79e69934bd98a389d58e0
Danny van Kooten <dannyvankooten@users.noreply.github.com>
2021-03-16 09:35:48 +0100

    update validation example to include isset & make it vegetarian

--- custom-form-validation.php
+++ custom-form-validation.php
@@ -1,15 +1,29 @@
 <?php
 
-// validate form request
+// validate form request (all forms)
+// this will validate that the field with name "BEST_VEGETABLE" is set has a value of "carrot"
 add_filter( 'hf_validate_form', function( $error_code, $form, $data ) {
-	if( $data['BEST_HAM'] !== 'iberico_ham' ) {
-		$error_code = 'invalid_ham_choice'; 
+	if( ! isset($data['BEST_VEGETABLE']) || $data['BEST_VEGETABLE'] !== 'carrot' ) {
+		$error_code = 'wrong_answer';
+	}
+
+	return $error_code;
+}, 10, 3 );
+
+// validate form request (form with ID 60 only)
+add_filter( 'hf_validate_form', function( $error_code, $form, $data ) {
+	if ($form->ID != 60) {
+		return $error_code;
+	}
+
+	if( ! isset($data['BEST_VEGETABLE']) || $data['BEST_VEGETABLE'] !== 'carrot' ) {
+		$error_code = 'wrong_answer';
 	}
 
 	return $error_code;
 }, 10, 3 );
 
 // register error message for our custom error code
-add_filter( 'hf_form_message_invalid_ham_choice', function( $message ) {
-    return 'Sorry, but iberico ham is really the best ham.';
+add_filter( 'hf_form_message_wrong_answer', function( $message ) {
+    return 'Sorry, but the best vegetable is a carrot!';
 });
